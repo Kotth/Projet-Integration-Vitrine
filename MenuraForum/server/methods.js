0b50feae7c7b5f95e2dcf13d8e354c49ca719c42
Arnaud Nilens <a.nilens@students.ephec.be>
2020-12-02 16:20:15 +0100

    added forum

---
+++ MenuraForum/server/methods.js
@@ -0,0 +1,41 @@
+Meteor.methods({
+    createThread: function(topicId, content){
+        check(topicId, String);
+        check(content, String);
+        const user = Meteor.user();
+        if (!user) {
+            throw new Meteor.Error("Vous n'êtes pas connecté.");
+        }
+        if (!content){
+            throw new Meteor.Error("Erreur!");
+        }
+
+        const thread = {
+            author: user.emails[0].address,
+            createdAt: new Date(),
+            topicId: topicId,
+            content: content
+        };
+        return Threads.insert(thread);
+    },
+    
+    createPost: function(threadId, content){
+        check(threadId, String);
+        check(content, String);
+        const user = Meteor.user();
+        if (!user) {
+            throw new Meteor.Error("Vous n'êtes pas connecté.");
+        }
+        if (!content) {
+            throw new Meteor.Error("Erreur!");
+        }
+
+        const post = {
+            author: user.emails[0].address,
+            createdAt: new Date(),
+            threadId: threadId,
+            content: content
+        };
+        return Posts.insert(post);
+    }    
+});
